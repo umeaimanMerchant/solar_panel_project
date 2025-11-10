585d2e8c8335ec058023e9a6c6539636ef86d95f
umeaimanMerchant <umeaiman.merchant17@vit.edu>
2025-11-10 08:58:04 -0500

    FastAPI app build is completed

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from src.Classifier.components.model_prediction import PredictionPipeline
+import uvicorn
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -13,34 +14,39 @@ def home(request: Request):
 
 @app.post("/predict", response_class=HTMLResponse)
 def predict(
+    request: Request,
     Acres: float = Form(...),
-    Distance_to_Substation_Miles_CAISO: float = Form(...),
-    Distance_to_Substation_Miles_GTET_100_Max_Voltage: float = Form(...),
-    Distance_to_Substation_Miles_GTET_200_Max_Voltage: float = Form(...),
+    Distance_CAISO: float = Form(...),
+    Distance_GTET100: float = Form(...),
+    Distance_GTET200: float = Form(...),
     Shape__Length: float = Form(...),
     Shape__Area: float = Form(...),
-    Percentile_GTET_100_Max_Voltage: str = Form(...),
-    Percentile_GTET_200_Max_Voltage: str = Form(...),
+    Percentile_GTET100: str = Form(...),
+    Percentile_GTET200: str = Form(...),
     Percentile_CAISO: str = Form(...),
     Install_Type: str = Form(...),
-    Urban_or_Rural: str = Form(...),
+    Urban_Rural: str = Form(...),
     County: str = Form(...)
 ):
     input_data = {
         "Acres": Acres,
-        "Distance_to_Substation_Miles_CAISO": Distance_to_Substation_Miles_CAISO,
-        "Distance_to_Substation_Miles_GTET_100_Max_Voltage": Distance_to_Substation_Miles_GTET_100_Max_Voltage,
-        "Distance_to_Substation_Miles_GTET_200_Max_Voltage": Distance_to_Substation_Miles_GTET_200_Max_Voltage,
+        "Distance to Substation (Miles) CAISO": Distance_CAISO,
+        "Distance to Substation (Miles) GTET 100 Max Voltage": Distance_GTET100,
+        "Distance to Substation (Miles) GTET 200 Max Voltage": Distance_GTET200,
         "Shape__Length": Shape__Length,
         "Shape__Area": Shape__Area,
-        "Percentile_GTET_100_Max_Voltage": Percentile_GTET_100_Max_Voltage,
-        "Percentile_GTET_200_Max_Voltage": Percentile_GTET_200_Max_Voltage,
-        "Percentile_CAISO": Percentile_CAISO,
-        "Install_Type": Install_Type,
-        "Urban_or_Rural": Urban_or_Rural,
+        "Percentile (GTET 100 Max Voltage)": Percentile_GTET100,
+        "Percentile (GTET 200 Max Voltage)": Percentile_GTET200,
+        "Percentile (CAISO)": Percentile_CAISO,
+        "Install Type": Install_Type,
+        "Urban or Rural": Urban_Rural,
         "County": County
     }
     pipeline = PredictionPipeline()
     prediction = pipeline.predict(input_data)
+    if prediction == 1:
+        prediction = "Is Fesible to have Solar Panels"
+    else:
+        prediction = "Is Not Feasible to have Solar Panels"
 
     return templates.TemplateResponse("index.html", {"request": request, "prediction": prediction})
